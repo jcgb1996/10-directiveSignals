5a4f36119f1a19c1a8bc99c18e85c44814c019db
carlos.gonzalez <carlos.gonzalez@guru-soft.com>
2024-12-02 17:23:36 -0500

    se subem cambios de manejo de señales

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, OnDestroy, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   standalone: false,
-  
+
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy {
+
+
+  public user=signal<User>({
+    id: 1,
+    email: 'aaa@bb.com',
+    first_name: 'jose',
+    last_name: 'carlos',
+    avatar: 'https://upload.wikimedia.org/wikipedia/en/8/86/Avatar_Aang.png'
+  });
+
+  public fullName = computed( ()=>`${this.user().first_name} ${this.user().last_name}` );
+
+  public userChangedEffect = effect( ()=> { console.log('userChangedEffect triggered'); } );
+
+  ngOnDestroy(): void {
+    this.userChangedEffect.destroy();
+  }
+
+  onFieldUpdated( field: keyof User, value:string ): void{
+    console.log({field, value});
+    //this.user.set( {
+    //  ...this.user(),
+    //  [field]: value
+    //});
+
+    //this.user.update( (current) =>({
+    //  ...current,
+    //  [field]: value
+    //}) );
+
+    this.user.update( (current)=> {
+
+      switch(field){
+        case 'email':
+          current.email = value;
+        break;
+        case 'avatar':
+            current.avatar = value;
+            break;
+         case 'first_name':
+              current.first_name = value;
+              break;
+         case 'last_name':
+          current.last_name = value;
+          break;
+          case 'id':
+           current.id = Number(value);
+           break;
+
+      }
+      return current;
+    } );
+  }
 
 }
--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { UserServiceService } from '../../services/user-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -15,6 +15,11 @@ export class UserInfoPageComponent implements OnInit {
 
   public currentUser = signal<User | undefined >(undefined);
   public userWasFound = signal(true);
+  public fullName= computed<string>( () => {
+    if(!this.currentUser()) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`
+  });
 
   ngOnInit(): void {
     this.loadUser( this.userId() );
@@ -26,10 +31,23 @@ export class UserInfoPageComponent implements OnInit {
     this.userId.set(id);
     this.currentUser.set(undefined);
 
-    this.userService.getUserById(id)
-    .subscribe( user => {
-      this.currentUser.set(user);
-    } )
+    //this.userService.getUserById(id)
+    //.subscribe( user => {
+    //  this.currentUser.set(user);
+    //} );
+
+    this.userService.getUserById(id).subscribe(
+      {
+        next: (value)  => {
+          this.currentUser.set(value);
+          this.userWasFound.set(true);
+        },
+        error: ()=> {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        }
+      }
+    );
 
   }
 }
